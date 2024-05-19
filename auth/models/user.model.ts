c6e47eadcb5d483b452d39c72bd56e2845db1d33
Arunkumar Murugesan <arunkumarmurugesan@Arunkumars-MacBook-Air.local>
2024-05-19 17:37:33 +0530

    added user model

--- auth/models/user.model.ts
+++ auth/models/user.model.ts
@@ -1,15 +1,78 @@
-import mongoose, { Document, Schema } from 'mongoose';
+import mongoose, { Document, Schema } from "mongoose";
+import bcrypt from "bcryptjs";
 
-export interface IUser extends Document {
-  email: string;
-  password: string;
+type Role = "user" | "admin"
+
+interface IUser extends Document{
+    firstName : string,
+    lastName : string,
+    username : string,
+    email : string,
+    hash_password : string,
+    role : Role,
+    contactNumber : string,
+    profilePicture : string,
+    fullName : string,
+    authenticate(password : string) : Promise<boolean>;
 }
 
-const userSchema: Schema = new Schema({
-  email: { type: String, required: true },
-  password: { type: String, required: true }
+const userSchema : Schema<IUser> = new Schema({
+    firstName : {
+        type : String,
+        require : true,
+        trim : true,
+        min : 3,
+        max : 20
+    },
+    lastName : {
+        type : String,
+        require : true,
+        trim : true,
+        min : 3,
+        max : 20
+    },
+    username: {
+        type: String,
+        require: true,
+        trim: true,
+        unique: true,
+        lowercase: true,
+        index: true,
+    },
+    email: {
+        type: String,
+        require: true,
+        trim: true,
+        unique: true,
+        lowercase: true,
+    },
+    hash_password: {
+        type: String,
+        require: true,
+    },
+    role: {
+        type: String,
+        enum: ["user", "admin"],
+        default: "user",
+    },
+    contactNumber: {
+        type: String,
+    },
+    profilePicture: {
+        type: String,
+    },
+},{ timestamps: true })
+
+//For get fullName from when we get data from database
+userSchema.virtual("fullName").get(function ( this : IUser) {
+    return `${this.firstName} ${this.lastName}`;
 });
 
-export const User = mongoose.model<IUser>('User', userSchema);
+// authenticate password function in the "Schema class"
+userSchema.methods.authenticate = async function (password : string) : Promise<boolean> {
+    return bcrypt.compare(password, this.hash_password)
+}
 
-export const db = { user: User };
+// Create and export the User model
+const User = mongoose.model<IUser>("User", userSchema);
+export default User;
